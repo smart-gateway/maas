e0e1228c762e0f70e4d7a516b25113f4ad2d81e6
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-10 15:38:12 -0800

    initial commit

---
+++ lib/puppet/functions/maas/get_machines.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+require "uri"
+require "net/http"
+
+url = URI("http://puppet.edge.lan:5240/MAAS/api/2.0/machines/")
+
+http = Net::HTTP.new(url.host, url.port);
+request = Net::HTTP::Get.new(url)
+request["Authorization"] = "OAuth oauth_consumer_key=\"wzKJH2WQJQ2QVYWjqZ\",oauth_token=\"SLFU2E9EqjMwUmJY4V\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"1673393600\",oauth_nonce=\"HH4LUSureA3\",oauth_version=\"1.0\",oauth_signature=\"%26XRnrfpxugwyFayLPu8Aqya4jMDxwMCSv\""
+
+response = http.request(request)
+puts response.read_body
---
+++ manifests/host.pp
@@ -0,0 +1,17 @@
+# @summary A short summary of the purpose of this defined type.
+#
+# A description of what this defined type does
+#
+# @example
+#   maas::host { 'namevar': }
+define maas::host (
+  String $mmas_server = 'localhost',
+  String $maas_api_base = '/MAAS/api/2.0/',
+  String $maas_consumer_key = '',
+  String $maas_token_key = '',
+  String $maas_token_secret = '',
+) {
+  # Test to see if I can even access the API
+  $result = maas::get_machines()
+  notify { "result: ${result}": }
+}
