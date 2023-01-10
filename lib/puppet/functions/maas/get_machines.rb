0e59ca7f207129861607cdfa20ce2c19e09934ab
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-10 15:54:50 -0800

    modify get_machines

--- lib/puppet/functions/maas/get_machines.rb
+++ lib/puppet/functions/maas/get_machines.rb
@@ -2,11 +2,17 @@
 require "uri"
 require "net/http"
 
-url = URI("http://puppet.edge.lan:5240/MAAS/api/2.0/machines/")
+Puppet::Functions.create_function(:'maas::get_machines') do
 
-http = Net::HTTP.new(url.host, url.port);
-request = Net::HTTP::Get.new(url)
-request["Authorization"] = "OAuth oauth_consumer_key=\"wzKJH2WQJQ2QVYWjqZ\",oauth_token=\"SLFU2E9EqjMwUmJY4V\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"1673393600\",oauth_nonce=\"HH4LUSureA3\",oauth_version=\"1.0\",oauth_signature=\"%26XRnrfpxugwyFayLPu8Aqya4jMDxwMCSv\""
+  def get_machines()
+    url = URI("http://puppet.edge.lan:5240/MAAS/api/2.0/machines/")
 
-response = http.request(request)
-return response.read_body
+    http = Net::HTTP.new(url.host, url.port);
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "OAuth oauth_consumer_key=\"wzKJH2WQJQ2QVYWjqZ\",oauth_token=\"SLFU2E9EqjMwUmJY4V\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"1673393600\",oauth_nonce=\"HH4LUSureA3\",oauth_version=\"1.0\",oauth_signature=\"%26XRnrfpxugwyFayLPu8Aqya4jMDxwMCSv\""
+
+    response = http.request(request)
+    return response.read_body
+  end
+end
+
