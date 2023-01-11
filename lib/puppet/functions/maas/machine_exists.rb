9f4634d965ec9590fc5ecda2fbc593660d7d9951
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-11 08:30:47 -0800

    add method to verify machine existence

--- lib/puppet/functions/maas/machine_exists.rb
+++ lib/puppet/functions/maas/machine_exists.rb
@@ -10,27 +10,20 @@ Puppet::Functions.create_function(:'maas::machine_exists') do
     param 'String', :machine_name
   end
   def machine_exists(server, consumer_token, auth_token, auth_signature, machine_name)
-    # url = URI("http://#{server}:5240/MAAS/api/2.0/machines/")
-    #
-    # http = Net::HTTP.new(url.host, url.port)
-    # nonce = rand(10 ** 30).to_s.rjust(30,'0')
-    # request = Net::HTTP::Post.new(url)
-    # request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"#{auth_signature}\""
-    # request["Content-Type"] = "application/json"
-    # body = {
-    #   "architecture": machine_architecture,
-    #   "min_hwe_kernel": "",
-    #   "mac_addresses": machine_mac_address,
-    #   "hostname": machine_name,
-    #   "description": machine_description,
-    #   "commission": true,
-    #   "testing_scripts": "none",
-    #   "domain": machine_domain
-    # }
-    # body.merge!(power_parameters)
-    # request.body = JSON.dump(body)
-    # response = http.request(request)
-    # return response
+    url = URI("http://#{server}:5240/MAAS/api/2.0/machines/")
+
+    http = Net::HTTP.new(url.host, url.port);
+    request = Net::HTTP::Get.new(url)
+    request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
+    response = http.request(request)
+    data = JSON.parse(response.read_body)
+
+    data.each do |host|
+      if host['hostname'] == machine_name
+        return true
+      end
+    end
+
     return false
   end
 end
