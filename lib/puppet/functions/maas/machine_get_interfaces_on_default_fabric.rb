0d549920737371e5d1eff27baec31944642cc42a
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-11 13:27:38 -0800

    fix function name

--- lib/puppet/functions/maas/machine_get_interfaces_on_default_fabric.rb
+++ lib/puppet/functions/maas/machine_get_interfaces_on_default_fabric.rb
@@ -1,13 +1,6 @@
 require "uri"
 require "net/http"
 
-class String
-  def is_i?
-    !!(self =~ /\A[-+]?[0-9]+\z/)
-  end
-end
-
-
 Puppet::Functions.create_function(:'maas::machine_get_ifaces_num_fabric') do
   dispatch :machine_get_ifaces_num_fabric do
     param 'String', :server
@@ -16,6 +9,13 @@ Puppet::Functions.create_function(:'maas::machine_get_ifaces_num_fabric') do
     param 'String', :auth_signature
     param 'String', :system_id
   end
+
+  class String
+    def is_i?
+      !!(self =~ /\A[-+]?[0-9]+\z/)
+    end
+  end
+
   def machine_get_ifaces_num_fabric(server, consumer_token, auth_token, auth_signature, system_id)
     url = URI("http://#{server}:5240/MAAS/api/2.0/machines/#{system_id}")
 
