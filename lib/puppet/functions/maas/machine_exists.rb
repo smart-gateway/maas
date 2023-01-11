829c04a37ad0290ad802fc31cc48d5a3f05a68a6
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-11 08:33:09 -0800

    add method to verify machine existence

--- lib/puppet/functions/maas/machine_exists.rb
+++ lib/puppet/functions/maas/machine_exists.rb
@@ -13,6 +13,7 @@ Puppet::Functions.create_function(:'maas::machine_exists') do
     url = URI("http://#{server}:5240/MAAS/api/2.0/machines/")
 
     http = Net::HTTP.new(url.host, url.port);
+    nonce = rand(10 ** 30).to_s.rjust(30,'0')
     request = Net::HTTP::Get.new(url)
     request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
     response = http.request(request)
