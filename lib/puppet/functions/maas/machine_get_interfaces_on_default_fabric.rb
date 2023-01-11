8d188ab4b1b7e3a3dc60611ac985963003ececcd
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-11 13:22:06 -0800

    fix function name

--- lib/puppet/functions/maas/machine_get_interfaces_on_default_fabric.rb
+++ lib/puppet/functions/maas/machine_get_interfaces_on_default_fabric.rb
@@ -8,15 +8,15 @@ class String
 end
 
 
-Puppet::Functions.create_function(:'maas::get_interfaces_on_default_fabric') do
-  dispatch :get_interfaces_on_default_fabric do
+Puppet::Functions.create_function(:'maas::machine_get_interfaces_on_default_fabric') do
+  dispatch :machine_get_interfaces_on_default_fabric do
     param 'String', :server
     param 'String', :consumer_token
     param 'String', :auth_token
     param 'String', :auth_signature
     param 'String', :system_id
   end
-  def get_interfaces_on_default_fabric(server, consumer_token, auth_token, auth_signature, system_id)
+  def machine_get_interfaces_on_default_fabric(server, consumer_token, auth_token, auth_signature, system_id)
     url = URI("http://#{server}:5240/MAAS/api/2.0/machines/#{system_id}")
 
     http = Net::HTTP.new(url.host, url.port);
