86a1b6a96cf86b06bc3ec2e117ed320bd3f059db
Benjamin Grewell <benjamin.grewell@intel.com>
2023-01-11 16:38:01 -0800

    comment new code

--- lib/puppet/functions/maas/interface_update_fabric.rb
+++ lib/puppet/functions/maas/interface_update_fabric.rb
@@ -1,30 +1,30 @@
-# frozen_string_literal: true
-require "uri"
-require "net/http"
-
-Puppet::Functions.create_function(:'maas::interface_update_fabric') do
-  dispatch :interface_update_fabric do
-    param 'String',  :server
-    param 'String',  :consumer_token
-    param 'String',  :auth_token
-    param 'String',  :auth_signature
-    param 'String',  :system_id
-    param 'Integer', :interface_id
-    param 'Integer', :vlan_id
-  end
-  def interface_update_fabric(server, consumer_token, auth_token, auth_signature, system_id, interface_id, vlan_id)
-    url = URI("http://#{server}:5240/MAAS/api/2.0/nodes/#{system_id}/interfaces/#{interface_id}/")
-
-    http = Net::HTTP.new(url.host, url.port)
-    nonce = rand(10 ** 30).to_s.rjust(30,'0')
-    request = Net::HTTP::Put.new(url)
-    request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
-    request["Content-Type"] = "application/json"
-    body = {
-      "vlan": vlan_id
-    }
-    request.body = JSON.dump(body)
-    response = http.request(request)
-    return response
-  end
-end
+# # frozen_string_literal: true
+# require "uri"
+# require "net/http"
+#
+# Puppet::Functions.create_function(:'maas::interface_update_fabric') do
+#   dispatch :interface_update_fabric do
+#     param 'String',  :server
+#     param 'String',  :consumer_token
+#     param 'String',  :auth_token
+#     param 'String',  :auth_signature
+#     param 'String',  :system_id
+#     param 'Integer', :interface_id
+#     param 'Integer', :vlan_id
+#   end
+#   def interface_update_fabric(server, consumer_token, auth_token, auth_signature, system_id, interface_id, vlan_id)
+#     url = URI("http://#{server}:5240/MAAS/api/2.0/nodes/#{system_id}/interfaces/#{interface_id}/")
+#
+#     http = Net::HTTP.new(url.host, url.port)
+#     nonce = rand(10 ** 30).to_s.rjust(30,'0')
+#     request = Net::HTTP::Put.new(url)
+#     request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
+#     request["Content-Type"] = "application/json"
+#     body = {
+#       "vlan": vlan_id
+#     }
+#     request.body = JSON.dump(body)
+#     response = http.request(request)
+#     return response
+#   end
+# end
--- lib/puppet/functions/maas/machine_get_interfaces_on_default_fabric.rb
+++ lib/puppet/functions/maas/machine_get_interfaces_on_default_fabric.rb
@@ -1,33 +1,33 @@
-require "uri"
-require "net/http"
-
-Puppet::Functions.create_function(:'maas::machine_get_ifaces_num_fabric') do
-  dispatch :machine_get_ifaces_num_fabric do
-    param 'String', :server
-    param 'String', :consumer_token
-    param 'String', :auth_token
-    param 'String', :auth_signature
-    param 'String', :system_id
-  end
-
-  def machine_get_ifaces_num_fabric(server, consumer_token, auth_token, auth_signature, system_id)
-    url = URI("http://#{server}:5240/MAAS/api/2.0/machines/#{system_id}")
-
-    http = Net::HTTP.new(url.host, url.port);
-    nonce = rand(10 ** 30).to_s.rjust(30,'0')
-    request = Net::HTTP::Get.new(url)
-    request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
-    response = http.request(request)
-    data = JSON.parse(response.read_body)
-
-    interface_ids = []
-    data['interface_set'].each do |interface|
-      fabric = interface['vlan']['fabric'].gsub("fabric-", "")
-      if fabric.to_i.to_s == fabric
-        interface_ids.append(interface['id'])
-      end
-    end
-
-    return interface_ids
-  end
-end
+# require "uri"
+# require "net/http"
+#
+# Puppet::Functions.create_function(:'maas::machine_get_ifaces_num_fabric') do
+#   dispatch :machine_get_ifaces_num_fabric do
+#     param 'String', :server
+#     param 'String', :consumer_token
+#     param 'String', :auth_token
+#     param 'String', :auth_signature
+#     param 'String', :system_id
+#   end
+#
+#   def machine_get_ifaces_num_fabric(server, consumer_token, auth_token, auth_signature, system_id)
+#     url = URI("http://#{server}:5240/MAAS/api/2.0/machines/#{system_id}")
+#
+#     http = Net::HTTP.new(url.host, url.port);
+#     nonce = rand(10 ** 30).to_s.rjust(30,'0')
+#     request = Net::HTTP::Get.new(url)
+#     request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
+#     response = http.request(request)
+#     data = JSON.parse(response.read_body)
+#
+#     interface_ids = []
+#     data['interface_set'].each do |interface|
+#       fabric = interface['vlan']['fabric'].gsub("fabric-", "")
+#       if fabric.to_i.to_s == fabric
+#         interface_ids.append(interface['id'])
+#       end
+#     end
+#
+#     return interface_ids
+#   end
+# end
