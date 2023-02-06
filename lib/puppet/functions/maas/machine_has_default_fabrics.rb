3e622f19b4c8e644d6c27016fb91ca25f31a6e34
Benjamin Grewell <benjamin.grewell@intel.com>
2023-02-06 11:33:14 -0800

    add function to check if system has default fabrics

---
+++ lib/puppet/functions/maas/machine_has_default_fabrics.rb
@@ -0,0 +1,14 @@
+Puppet::Functions.create_function(:'maas::machine_has_default_fabrics') do
+  dispatch :machine_has_default_fabrics do
+    param 'String', :server
+    param 'String', :consumer_token
+    param 'String', :auth_token
+    param 'String', :auth_signature
+    param 'String', :system_id
+  end
+
+  def machine_has_default_fabrics(server, consumer_token, auth_token, auth_signature, system_id)
+    interfaces = call_function('machine_get_unidentified_interfaces', server, consumer_token, auth_token, auth_signature, system_id)
+    return interfaces.any?
+  end
+end
