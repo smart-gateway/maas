1f5052fb1c6692a9cf102b2c0ded61ad3cb1d9f1
Benjamin Grewell <benjamin.grewell@intel.com>
2023-02-06 11:12:27 -0800

    debug

--- lib/puppet/functions/maas/machine_get_unidentified_interfaces.rb
+++ lib/puppet/functions/maas/machine_get_unidentified_interfaces.rb
@@ -20,7 +20,7 @@ Puppet::Functions.create_function(:'maas::machine_get_unidentified_interfaces') 
     request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
     response = http.request(request)
     data = JSON.parse(response.read_body)
-    Puppet.send("warning", "interface_set: #{JSON.pretty_generate(data['interface_set'])}")
+    Puppet.send("warning", "interface_set json: #{JSON.pretty_generate(data['interface_set'])}")
     interface_ids = []
     if !data.nil? && data.key?('interface_set')
       data['interface_set'].each do |interface|
