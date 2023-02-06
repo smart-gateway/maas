62b478233a0bf815db6daf38465525e89b6e175a
Benjamin Grewell <benjamin.grewell@intel.com>
2023-02-06 11:37:03 -0800

    add check to ensure default fabrics have been removed

--- lib/puppet/functions/maas/machine_has_default_fabrics.rb
+++ lib/puppet/functions/maas/machine_has_default_fabrics.rb
@@ -8,7 +8,7 @@ Puppet::Functions.create_function(:'maas::machine_has_default_fabrics') do
   end
 
   def machine_has_default_fabrics(server, consumer_token, auth_token, auth_signature, system_id)
-    interfaces = call_function('machine_get_unidentified_interfaces', server, consumer_token, auth_token, auth_signature, system_id)
+    interfaces = call_function('maas::machine_get_unidentified_interfaces', server, consumer_token, auth_token, auth_signature, system_id)
     return interfaces.any?
   end
 end
