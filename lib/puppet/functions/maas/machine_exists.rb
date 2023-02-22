44db4f6e40847d84d11b69f032cb4f1d6ba7e000
Benjamin Grewell <benjamin.grewell@intel.com>
2023-02-22 13:45:22 -0800

    modify to use all returned ip addresses

--- lib/puppet/functions/maas/machine_exists.rb
+++ lib/puppet/functions/maas/machine_exists.rb
@@ -1,5 +1,6 @@
 require "uri"
 require "net/http"
+require 'resolv'
 
 Puppet::Functions.create_function(:'maas::machine_exists') do
   dispatch :machine_exists do
@@ -10,20 +11,43 @@ Puppet::Functions.create_function(:'maas::machine_exists') do
     param 'String', :machine_name
   end
   def machine_exists(server, consumer_token, auth_token, auth_signature, machine_name)
-    url = URI("http://#{server}:5240/MAAS/api/2.0/machines/")
-    http = Net::HTTP.new(url.host, url.port);
-    nonce = rand(10 ** 30).to_s.rjust(30,'0')
-    request = Net::HTTP::Get.new(url)
-    request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
-    response = http.request(request)
-    data = JSON.parse(response.read_body)
 
-    data.each do |host|
-      if host['hostname'] == machine_name
-        return true
+    addrs = Resolv::DNS.new.getaddresses(server).sort { |a,b| b.class.to_s <=> a.class.to_s }.collect { |ip| ip.to_s }
+    begin
+      url = URI("http://#{addrs.shift}:5240/MAAS/api/2.0/machines/")
+      http = Net::HTTP.new(url.host, url.port);
+      nonce = rand(10 ** 30).to_s.rjust(30,'0')
+      request = Net::HTTP::Get.new(url)
+      request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
+      response = http.request(request)
+      data = JSON.parse(response.read_body)
+      data.each do |host|
+        if host['hostname'] == machine_name
+          return true
+        end
       end
+
+      return false
+    rescue Timeout::Error => e
+      retry unless addrs.empty?
+      raise e
     end
 
-    return false
+
+    # url = URI("http://#{server}:5240/MAAS/api/2.0/machines/")
+    # http = Net::HTTP.new(url.host, url.port);
+    # nonce = rand(10 ** 30).to_s.rjust(30,'0')
+    # request = Net::HTTP::Get.new(url)
+    # request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
+    # response = http.request(request)
+    # data = JSON.parse(response.read_body)
+
+    # data.each do |host|
+    #   if host['hostname'] == machine_name
+    #     return true
+    #   end
+    # end
+    #
+    # return false
   end
 end
