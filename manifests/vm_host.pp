948ae5ec03de2ea4f90e9897e15bb28c3dd1ba97
Benjamin Grewell <benjamin.grewell@intel.com>
2023-03-09 11:20:18 -0800

    remove unused vm_host class

--- manifests/vm_host.pp
+++
@@ -1,9 +0,0 @@
-# @summary A short summary of the purpose of this defined type.
-#
-# A description of what this defined type does
-#
-# @example
-#   maas::vm_host { 'namevar': }
-define maas::vm_host (
-) {
-}
