b30de68bc50d9ec4167d608ca74318f9892fcded
Benjamin Grewell <benjamin.grewell@intel.com>
2023-05-01 15:46:39 -0700

    move to dtac interface

--- lib/puppet/functions/maas/machine_exists.rb
+++ lib/puppet/functions/maas/machine_exists.rb
@@ -14,20 +14,32 @@ Puppet::Functions.create_function(:'maas::machine_exists') do
   def machine_exists(server, consumer_token, auth_token, auth_signature, machine_name, module_debug)
 
     addr = Resolv.getaddress(server)
-    url = URI("http://#{addr}:5240/MAAS/api/2.0/machines/?hostname=#{machine_name}")
+    #url = URI("http://#{addr}:5240/MAAS/api/2.0/machines/?hostname=#{machine_name}")
+    url = URI("http://#{addr}:8080/plugins/maas/machine/id?host=#{machine_name}")
     http = Net::HTTP.new(url.host, url.port);
     nonce = rand(10 ** 30).to_s.rjust(30,'0')
     request = Net::HTTP::Get.new(url)
-    request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
+    #request["Authorization"] = "OAuth oauth_consumer_key=\"#{consumer_token}\",oauth_token=\"#{auth_token}\",oauth_signature_method=\"PLAINTEXT\",oauth_timestamp=\"#{Time.now.to_i}\",oauth_nonce=\"#{nonce}\",oauth_version=\"1.0\",oauth_signature=\"%26#{auth_signature}\""
     response = http.request(request)
-    data = JSON.parse(response.read_body)
-    data.each do |host|
-      if host['hostname'] == machine_name
-        Puppet.send("info", "found host: #{host}") if module_debug
-        return true
-      end
+
+    begin
+      data = JSON.parse(response.read_body)
+      return data['machine'] == machine_name
+    rescue JSON::ParserError => e
+      puts "Error parsing JSON: #{e}"
+      return false
     end
+
     return false
+
+    # data = JSON.parse(response.read_body)
+    # data.each do |host|
+    #   if host['hostname'] == machine_name
+    #     Puppet.send("info", "found host: #{host}") if module_debug
+    #     return true
+    #   end
+    # end
+    # return false
     # url = URI("http://#{server}:5240/MAAS/api/2.0/machines/")
     # http = Net::HTTP.new(url.host, url.port);
     # nonce = rand(10 ** 30).to_s.rjust(30,'0')
